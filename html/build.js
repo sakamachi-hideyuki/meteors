2e959736fbaaff6a69b76c349cca5e28a11c412d
sakamachi.hideyuki <sakamachi.hideyuki@gmail.com>
2023-04-08 23:54:14 +0900

    固定ヘッダや下部のリンクの文字、スタイルを修正

--- html/build.js
+++ html/build.js
@@ -285,7 +285,7 @@ class Builder {
       });
     const indexPage = {
       id: "index",
-      title: "先頭頁",
+      title: "先頭ページ",
       filename: "index.html",
     };
     pages.splice(0, 0, indexPage);
@@ -359,7 +359,7 @@ ${tocHtml}
   createNextPageLinkHtml(nextPage) {
     return nextPage == null
       ? ""
-      : `<a class="next-page" href="${nextPage.filename}">次頁&nbsp;&gt;&nbsp;${nextPage.title}</a>`;
+      : `<a class="next-page" href="${nextPage.filename}">次ページ&nbsp;&gt;&nbsp;${nextPage.title}</a>`;
   }
 
   createStickyHeaderHtml(prevPage, curPage, nextPage) {
@@ -368,19 +368,19 @@ ${tocHtml}
 <nav>
 <ul>
 <li>
-<a href="index.html">≪&nbsp;先頭頁</a>
+<a href="index.html">≪&nbsp;先頭ページ</a>
 </li>
 <li>
 <a href="index.html#toc-${curPage.id}">目次</a>
 </li>
 <li>
-<a href="${prevPage.filename}">&lt;&nbsp;前頁</a>
+<a href="${prevPage.filename}">&lt;&nbsp;前ページ</a>
 </li>
 <li>
 ${
   nextPage == undefined
     ? ""
-    : `<a href="${nextPage.filename}">次頁&nbsp;&gt;</a>`
+    : `<a href="${nextPage.filename}">次ページ&nbsp;&gt;</a>`
 }
 </li>
 </ul>
