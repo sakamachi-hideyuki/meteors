7a8eb339dc2ec8d0076fb9749f54a01a5dbc978a
sakamachi.hideyuki <sakamachi.hideyuki@gmail.com>
2023-12-26 07:58:49 +0900

    リファクタリング

--- html/modules/page-linker.js
+++ html/modules/page-linker.js
@@ -1,7 +1,5 @@
 export class PageLinker {
   static link(pages) {
-    const preface = pages.find((p) => p.id === "preface");
-    const appendix = pages.find((p) => p.id === "appendix");
     for (let i = 0; i < pages.length; i++) {
       const page = pages[i];
       // 目次、先頭ページ、序文、付録、改版履歴のページや、コンテンツなしのページはスキップ
@@ -15,106 +13,122 @@ export class PageLinker {
       ) {
         continue;
       }
-      // "序文"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(/序文/g, (s) => {
-        return `<a href="${preface.filename}">${s}</a>`;
+      this.#linkPreface(pages, page);
+      this.#linkAppendix(pages, page);
+      this.#linkChapter(pages, page);
+      this.#linkSection(pages, page, i);
+      this.#linkSupplement(pages, page);
+    }
+  }
+
+  static #linkPreface(pages, page) {
+    // "序文"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(/序文/g, (s) => {
+      const target = pages.find((p) => p.id === "preface");
+      return `<a href="${target.filename}">${s}</a>`;
+    });
+  }
+
+  static #linkAppendix(pages, page) {
+    // "付録参照"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(/付録参照/g, (s) => {
+      const target = pages.find((p) => p.id === "appendix");
+      return `<a href="${target.filename}">${s}</a>`;
+    });
+    // 「付録　神名末尾のパターン」以外のページでは最初の"神名末尾のパターン"をリンク化
+    if (page.id !== "appendix") {
+      page.contentHtml = page.contentHtml.replace(/神名末尾のパターン/, (s) => {
+        const target = pages.find((p) => p.id === "appendix");
+        return `<a href="${target.filename}">${s}</a>`;
       });
-      // "付録参照"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(/付録参照/g, (s) => {
-        return `<a href="${appendix.filename}">${s}</a>`;
-      });
-      // 「付録　神名末尾のパターン」以外のページでは"神名末尾のパターン"をリンク化
-      if (page.id !== "appendix") {
-        page.contentHtml = page.contentHtml.replace(
-          /神名末尾のパターン/,
-          (s) => {
-            return `<a href="${appendix.filename}">${s}</a>`;
-          }
-        );
+    }
+  }
+
+  static #linkChapter(pages, page) {
+    // "「～の章」で"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(
+      /「([^<>「」]+の章)」で/g,
+      (s, p1) => {
+        const target = pages.find((p) => p.h2Title === p1 && p.h3Title === "");
+        if (target === undefined) {
+          console.error(`createLinks: ${s} not found.`);
+          return s;
+        }
+        return `<a href="${target.filename}">「${p1}」</a>で`;
       }
-      // "「～の章」～の項"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(
-        /「([^<>「」]+の章)」([^<>「」項]+)の項/g,
-        (s, p1, p2) => {
-          const index = p2.indexOf("、");
-          if (index !== -1) {
-            p2 = p2.substring(0, index);
-          }
-          const target = pages.find(
-            (p) => p.h2Title === p1 && p.h3Title === p2
-          );
-          if (target === undefined) {
-            console.error(`createLinks: ${s} not found.`);
-            return s;
-          }
-          return `<a href="${target.filename}">${s}</a>`;
-        }
+    );
+    // "本章冒頭"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(/本章冒頭/g, (s) => {
+      const target = pages.find(
+        (p) => p.h2Title === page.h2Title && p.h3Title === ""
       );
-      // "本章の～の項"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(
-        /本章の([^<>「」項]+)の項/g,
-        (s, p1) => {
-          const index = p1.indexOf("、");
-          if (index !== -1) {
-            p1 = p1.substring(0, index);
-          }
-          const target = pages.find(
-            (p) => p.h2Title === page.h2Title && p.h3Title === p1
-          );
-          if (target === undefined) {
-            console.error(`createLinks: ${s} not found.`);
-            return s;
-          }
-          return `<a href="${target.filename}">${s}</a>`;
+      if (target === undefined) {
+        console.error(`createLinks: ${s} not found.`);
+        return s;
+      }
+      return `<a href="${target.filename}">${s}</a>`;
+    });
+  }
+
+  static #linkSection(pages, page, pageIndex) {
+    // "「～の章」～の項"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(
+      /「([^<>「」]+の章)」([^<>「」項]+)の項/g,
+      (s, p1, p2) => {
+        const index = p2.indexOf("、");
+        if (index !== -1) {
+          p2 = p2.substring(0, index);
         }
-      );
-      // "「～の章」で"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(
-        /「([^<>「」]+の章)」で/g,
-        (s, p1) => {
-          const target = pages.find(
-            (p) => p.h2Title === p1 && p.h3Title === ""
-          );
-          if (target === undefined) {
-            console.error(`createLinks: ${s} not found.`);
-            return s;
-          }
-          return `<a href="${target.filename}">「${p1}」</a>で`;
+        const target = pages.find((p) => p.h2Title === p1 && p.h3Title === p2);
+        if (target === undefined) {
+          console.error(`createLinks: ${s} not found.`);
+          return s;
         }
-      );
-      // "本章冒頭"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(/本章冒頭/g, (s) => {
+        return `<a href="${target.filename}">${s}</a>`;
+      }
+    );
+    // "本章の～の項"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(
+      /本章の([^<>「」項]+)の項/g,
+      (s, p1) => {
+        const index = p1.indexOf("、");
+        if (index !== -1) {
+          p1 = p1.substring(0, index);
+        }
         const target = pages.find(
-          (p) => p.h2Title === page.h2Title && p.h3Title === ""
+          (p) => p.h2Title === page.h2Title && p.h3Title === p1
         );
         if (target === undefined) {
           console.error(`createLinks: ${s} not found.`);
           return s;
         }
         return `<a href="${target.filename}">${s}</a>`;
-      });
-      // "前項"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(/前項/g, (s) => {
-        const target = pages[i - 1];
-        return `<a href="${target.filename}">${s}</a>`;
-      });
-      // "次項"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(/次項/g, (s) => {
-        const target = pages[i + 1];
-        return `<a href="${target.filename}">${s}</a>`;
-      });
-      // "「補足　～」"をリンク化
-      page.contentHtml = page.contentHtml.replaceAll(
-        /「(補足　[^<>「」]+)」/g,
-        (s, p1) => {
-          const target = pages.find((p) => p.h3Title === p1);
-          if (target === undefined) {
-            console.error(`createLinks: ${s} not found.`);
-            return s;
-          }
-          return `<a href="${target.filename}">${s}</a>`;
+      }
+    );
+    // "前項"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(/前項/g, (s) => {
+      const target = pages[pageIndex - 1];
+      return `<a href="${target.filename}">${s}</a>`;
+    });
+    // "次項"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(/次項/g, (s) => {
+      const target = pages[pageIndex + 1];
+      return `<a href="${target.filename}">${s}</a>`;
+    });
+  }
+
+  static #linkSupplement(pages, page) {
+    // "「補足　～」"をリンク化
+    page.contentHtml = page.contentHtml.replaceAll(
+      /「(補足　[^<>「」]+)」/g,
+      (s, p1) => {
+        const target = pages.find((p) => p.h3Title === p1);
+        if (target === undefined) {
+          console.error(`createLinks: ${s} not found.`);
+          return s;
         }
-      );
-    }
+        return `<a href="${target.filename}">${s}</a>`;
+      }
+    );
   }
 }
