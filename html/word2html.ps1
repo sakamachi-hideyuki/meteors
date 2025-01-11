8a22a60ec1c6c82212e0cb02fccaf91ef9c4f6df
sakamachi.hideyuki <sakamachi.hideyuki@gmail.com>
2025-01-11 10:05:53 +0900

    HTML形式で保存するとWebレイアウトに切り替わるので、元のレイアウトに戻すよう自動化

--- html/word2html.ps1
+++ html/word2html.ps1
@@ -1,7 +1,11 @@
 Param($wordFile, $htmlFile)
 $app = New-Object -ComObject Word.Application
 $doc = $app.Documents.Open($wordFile)
-$doc.WebOptions.Encoding = 65001
+# HTML形式で保存するとWebレイアウトに切り替わるので、元のレイアウトを保持しておく
+$viewType = $doc.ActiveWindow.View.Type
+$doc.WebOptions.Encoding = 65001 # UTF-8
 $doc.SaveAs2($htmlFile, 10)
+# HTML形式で保存するとWebレイアウトに切り替わるので、元のレイアウトに戻す
+$doc.ActiveWindow.View.Type = $viewType
 $doc.Close()
 $app.Quit()
