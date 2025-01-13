62a96730b9ec643b6ec1cc8e6867bc1944cc2b6e
sakamachi.hideyuki <sakamachi.hideyuki@gmail.com>
2025-01-13 22:47:44 +0900

    図のデザイン調整

--- images/fig-subarunoko/fig-subarunoko.js
+++ images/fig-subarunoko/fig-subarunoko.js
@@ -6,27 +6,12 @@ class FigSubarunoko {
         font-size: 0;
         margin: 0;
       }
-      u {
-        text-underline-offset: 8px;
-      }
       #text > div {
         color: #000;
         font-family: serif;
         font-size: 50px;
-        font-weight: bold;
         position: absolute;
         text-align: center;
-        white-space: nowrap;
-      }
-      #bubble > div {
-        background-color: #fff;
-        border: 2px solid #000;
-        color: #000;
-        font-family: serif;
-        font-size: 50px;
-        font-weight: bold;
-        padding: 40px 50px 20px 50px;
-        position: absolute;
         white-space: nowrap;
       }
       svg {
@@ -57,16 +42,54 @@ class FigSubarunoko {
         fill="none"
       />
       <line
+        x1="1950"
+        y1="190"
+        x2="2010"
+        y2="190"
+      />
+      <line
+        x1="1950"
+        y1="210"
+        x2="2010"
+        y2="210"
+      />
+      <line
         x1="1980"
         y1="210"
         x2="1980"
         y2="770"
+      />
+
+      <line
+        x1="1250"
+        y1="190"
+        x2="1310"
+        y2="190"
+      />
+      <line
+        x1="1250"
+        y1="210"
+        x2="1310"
+        y2="210"
       />
       <line
         x1="1280"
         y1="210"
         x2="1280"
         y2="935"
+      />
+
+      <line
+        x1="550"
+        y1="190"
+        x2="610"
+        y2="190"
+      />
+      <line
+        x1="550"
+        y1="210"
+        x2="610"
+        y2="210"
       />
       <line
         x1="580"
@@ -81,9 +104,6 @@ class FigSubarunoko {
       </div>
       <div style="left: 1830px; top: 100px; writing-mode: vertical-rl;">
         <ruby>素戔嗚<rp>(</rp><rt>スサノオ</rt><rp>)</rp></ruby>の剣（<ruby>蛇韓鋤之剣<rp>(</rp><rt>おろちのからすきのつるぎ</rt><rp>)</rp></ruby>）<br>
-      </div>
-      <div style="left: 1930px; top: 120px; font-size: 100px;">
-        ＝
       </div>
       <div style="left: 2030px; top: 100px; writing-mode: vertical-rl;">
         <ruby>五百箇御統<rp>(</rp><rt>いおつのみすまる</rt><rp>)</rp></ruby><br>
@@ -100,7 +120,7 @@ class FigSubarunoko {
         <ruby>天津彦根<rp>(</rp><rt>アマツヒコネ</rt><rp>)</rp></ruby>命<br>
         <ruby>活津彦根<rp>(</rp><rt>イクツヒコネ</rt><rp>)</rp></ruby>命<br>
         <ruby>熊野櫲樟日<rp>(</rp><rt>クマノクスヒ</rt><rp>)</rp></ruby>命<br>
-        <ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神
+        <strong><ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神</strong>
       </div>
 
       <div style="left: 1080px; top: 100px; writing-mode: vertical-rl; font-size: 40px;">
@@ -108,9 +128,6 @@ class FigSubarunoko {
       </div>
       <div style="left: 1130px; top: 100px; writing-mode: vertical-rl;">
         <ruby>伊奘諾<rp>(</rp><rt>イザナキ</rt><rp>)</rp></ruby>の剣（<ruby>伊都之尾羽張<rp>(</rp><rt>いつのおはばり</rt><rp>)</rp></ruby>）<br>
-      </div>
-      <div style="left: 1230px; top: 120px; font-size: 100px;">
-        ＝
       </div>
       <div style="left: 1330px; top: 100px; writing-mode: vertical-rl;">
         <ruby>五百箇磐石<rp>(</rp><rt>いおついわむら</rt><rp>)</rp></ruby>（<ruby>湯津石村<rp>(</rp><rt>ゆついわむら</rt><rp>)</rp></ruby>）<br>
@@ -124,9 +141,9 @@ class FigSubarunoko {
         <ruby>磐筒男<rp>(</rp><rt>イワツツノオ</rt><rp>)</rp></ruby>神（<ruby>石筒之男<rp>(</rp><rt>イワツツノオ</rt><rp>)</rp></ruby>神）<br>
         <ruby>磐筒女<rp>(</rp><rt>イワツツノメ</rt><rp>)</rp></ruby>神<br>
         <ruby>経津主<rp>(</rp><rt>フツヌシ</rt><rp>)</rp></ruby>神<br>
-        <ruby>甕速日<rp>(</rp><rt>ミカハヤヒ</rt><rp>)</rp></ruby>神<br>
-        <ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神（<ruby>樋速日<rp>(</rp><rt>ヒハヤヒ</rt><rp>)</rp></ruby>神）<br>
-        <ruby>武甕槌<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神（<ruby>建御雷之男<rp>(</rp><rt>タケミカヅチノオ</rt><rp>)</rp></ruby>神）
+        <strong><ruby>甕速日<rp>(</rp><rt>ミカハヤヒ</rt><rp>)</rp></ruby>神</strong><br>
+        <strong><ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神（<ruby>樋速日<rp>(</rp><rt>ヒハヤヒ</rt><rp>)</rp></ruby>神）</strong><br>
+        <strong><ruby>武甕槌<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神（<ruby>建御雷之男<rp>(</rp><rt>タケミカヅチノオ</rt><rp>)</rp></ruby>神）</strong>
       </div>
 
       <div style="left: 380px; top: 100px; writing-mode: vertical-rl; font-size: 40px;">
@@ -135,9 +152,6 @@ class FigSubarunoko {
       <div style="left: 430px; top: 100px; writing-mode: vertical-rl;">
         <ruby>伊奘諾<rp>(</rp><rt>イザナキ</rt><rp>)</rp></ruby>の剣の神（<ruby>伊都之尾羽張<rp>(</rp><rt>イツノオハバリ</rt><rp>)</rp></ruby>神、<ruby>稜威雄走<rp>(</rp><rt>イツノオバシリ</rt><rp>)</rp></ruby>神）
       </div>
-      <div style="left: 530px; top: 120px; font-size: 100px;">
-        ＝
-      </div>
       <div style="left: 630px; top: 100px; writing-mode: vertical-rl;">
         <ruby>天岩戸<rp>(</rp><rt>あまのいわと</rt><rp>)</rp></ruby>
       </div>
@@ -145,9 +159,9 @@ class FigSubarunoko {
         <ruby>昴<rp>(</rp><rt>すばる</rt><rp>)</rp></ruby>
       </div>
       <div style="left: 280px; top: 1360px; width: 600px;">
-        <ruby>甕速日<rp>(</rp><rt>ミカハヤヒ</rt><rp>)</rp></ruby>神<br>
-        <ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神<br>
-        <ruby>武甕槌<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神（<ruby>建御雷<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神）
+        <strong><ruby>甕速日<rp>(</rp><rt>ミカハヤヒ</rt><rp>)</rp></ruby>神</strong><br>
+        <strong><ruby>熯速日<rp>(</rp><rt>ヒノハヤヒ</rt><rp>)</rp></ruby>神</strong><br>
+        <strong><ruby>武甕槌<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神（<ruby>建御雷<rp>(</rp><rt>タケミカヅチ</rt><rp>)</rp></ruby>神）</strong>
       </div>
     </div>`;
 
